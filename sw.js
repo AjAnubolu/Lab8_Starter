96b9f9b596f3eeb5418bfee90cf0ab869ac756bd
Ajay Anubolu <124525760+AjAnubolu@users.noreply.github.com>
2026-05-28 17:39:12 -0700

    Refactor sw.js by removing unnecessary comments
    Removed comments and cleaned up the service worker code.

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-// sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
-
 const CACHE_NAME = 'lab-8-starter';
 
-// The same recipe URLs requested by main.js. They are pre-cached on install so
-// the recipe data is available even on the very first offline visit.
 const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
   'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
@@ -14,42 +9,21 @@ const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
 ];
 
-// Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. Add all of the URLs from RECIPE_URLS so that they are added to the
-      //     cache when the ServiceWorker is installed.
       return cache.addAll(RECIPE_URLS);
     })
   );
 });
 
-// Activates the service worker
 self.addEventListener('activate', function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-// Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. Respond to the event by opening the cache using the name we gave
-  //     above (CACHE_NAME).
   event.respondWith(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B8. If the request is in the cache, return the cached version.
-      //     Otherwise fetch the resource, add it to the cache, and return the
-      //     network response.
       return cache.match(event.request).then(function (cachedResponse) {
         return (
           cachedResponse ||
